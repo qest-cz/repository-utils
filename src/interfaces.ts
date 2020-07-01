65e3be373f2de75d5f1ef65e0387e8f66e232517
Tomas Hampl <tomas.hampl@ack.ee>
2020-07-01 09:36:13 +0200

    🚚 Rename parameters

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -7,13 +7,13 @@ export interface IGetAllByParams<T> {
 }
 
 export interface IRepository<T> {
-    getAllByParams(args: IGetAllByParams<T>): Promise<T[]>;
+    getAllByParams(params: IGetAllByParams<T>): Promise<T[]>;
     getById(id: any): Promise<T>;
-    getByParams(t: Partial<T>): Promise<T>;
-    countByParams(t: Partial<T>): Promise<number>;
-    insert(t: Partial<T>): Promise<T>;
-    updateById(id: any, t: Partial<T>): Promise<T>;
-    updateByParams(w: Partial<T>, t: Partial<T>): Promise<number>;
-    upsert(w: Partial<T>, t: Partial<T>): Promise<number>;
-    deleteByParams(t: Partial<T>): Promise<number>;
+    getByParams(params: Partial<T>): Promise<T>;
+    countByParams(params: Partial<T>): Promise<number>;
+    insert(entity: Partial<T>): Promise<T>;
+    updateById(id: any, data: Partial<T>): Promise<T>;
+    updateByParams(params: Partial<T>, data: Partial<T>): Promise<number>;
+    upsert(searchParams: Partial<T>, data: Partial<T>): Promise<number>;
+    deleteByParams(params: Partial<T>): Promise<number>;
 }
